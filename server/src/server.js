8b179fda8790b8e2bbf34699797428888ad290a6
Shivam Siddharth <shivam.siddharth04@gmail.com>
2026-04-23 21:27:30 +0530

    backend ready for render

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './app.js';
+import cors from "cors";
+
+app.use(cors({
+  origin: "*"
+}));
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("API KEY:", process.env.GEMINI_API_KEY);
